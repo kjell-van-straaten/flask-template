f1808b5898f52895b1b94809677aac1888af1de4
kjellvs <64495619+kjellvs@users.noreply.github.com>
2021-09-23 15:55:37 +0200

    db connection

---
+++ venv/app/functions.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+def connect_db():
+    test_str = "7yPxFfQLlq1ssIIm"
+    access_line = "mongodb+srv://admin:{}@cluster0.mdtqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(test_str)
+    client = MongoClient(access_line)
+    db = client.get_database("TournamentApp")
+    return db
+
+def create_record(table, attributes: list):
+    keys = table.find_one().keys()
+    
+    new_entry= dict()
+
+    index = 0
+
+    for key in keys:
+        if key != '_id':
+            new_entry[key]=attributes[index]
+            index += 1
+
+    result = table.insert_one(new_entry)
+    return result
--- venv/app/main.py
+++ venv/app/main.py
@@ -1,5 +1,19 @@
+from app.functions import create_record
 from flask import Flask
+
+from functions import *
+
 app= Flask(__name__)
 @app.route('/')
 def index():
-  return "<h1>Faka</h1>"
+  return "<h1>Faka ambre</h1>"
+
+db = connect_db()
+
+accounts = db.Account
+bets = db.Bet
+matches = db.Match
+tournaments = db.Tournaments
+
+create_record(accounts, ['BitchBoy2','No', '789'])
+
