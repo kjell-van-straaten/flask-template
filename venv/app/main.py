bb0f2f49393fcdc1b81a4567c6536cc13289c683
kjellvs <64495619+kjellvs@users.noreply.github.com>
2021-09-24 17:15:02 +0200

    added find tournament

--- venv/app/main.py
+++ venv/app/main.py
@@ -12,10 +12,7 @@ db = connect_db()
 accounts = db.Account
 bets = db.Bet
 matches = db.Match
-tournaments = db.Tournaments
-
-#string = str(accounts.find_one({"name": 'Kjellvs19'})['_id'])
-#print(accounts.find_one({"_id": ObjectId(string)}))
+tournaments = db.Tournament
 
 #establish encryption
 fernet = encryption()
@@ -28,10 +25,19 @@ def before_request():
     user = User(current_user['_id'],current_user['name'],current_user['password'])
     g.user = user
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 #landing page; should have functionality on finding a tournament to bet on, or logging in for creating/overviewing owned tournaments.
 def index():
-  return render_template('home.html')
+    if request.method == 'POST':
+      tournament_name = request.form['search-term']
+      tournament = tournaments.find_one({"name": tournament_name})
+      if tournament:
+        return redirect(url_for('bet_tournament', name=tournament_name))
+      else:
+        return render_template('home.html', failed=True)
+
+    else:
+      return render_template('home.html')
 
 @app.route('/navbar')
 def navbar():
@@ -46,7 +52,6 @@ def logout():
   print(session['user_id'] + 'logged out')
   session.pop('user_id', None)
   return redirect('/login')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -92,7 +97,7 @@ def signup():
 
 #TODO2: build landing page post-login, overviewing tournaments
 @app.route('/tournaments')
-def tournaments():
+def tournament_page():
   if not g.user:
     return redirect('/login')
   return render_template('tournaments.html')
@@ -113,9 +118,9 @@ def tournament_overview():
 
 #TODO4: build betting page for selected tournament, pulls active (in the next 7 days) matches, 
 #and optionality to fill in scores which can be posted (only if owner of tournament!)
-@app.route('/bet/{tournament_name}')
-def bet_tournament():
-  return render_template('betTournament.html', tournament_name = "clicked tournament")
+@app.route('/bet/<name>')
+def bet_tournament(name):
+  return render_template('betTournament.html', name = name)
 
 #fernet.encrypt(password.encode())
 
