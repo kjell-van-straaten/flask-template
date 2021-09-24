dfaf70a97024161332f6d2d5019cf4b2f993aad2
Bourdier, Ambre <bambre@amazon.lu>
2021-09-24 10:01:43 +0200

    added session functionality

--- venv/app/main.py
+++ venv/app/main.py
@@ -1,20 +1,77 @@
-from flask import Flask, render_template
-from app.functions import *
+from flask import Flask, render_template, request,g, redirect, session, Response, url_for
+#from app.functions import *
+from functions import *
 
 app= Flask(__name__)
+app.secret_key = 'courgette'
+
+class User:
+  def __init__(self, id, username, password):
+    self.id = id
+    self.username = username
+    self.password = password
+
+  def __repr__(self):
+    return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='admin', password='123'))
+
+@app.before_request
+def before_request():
+  g.user = None
+  if 'user_id' in session:
+    user = [x for x in users if x.id == session['user_id']][0]
+    g.user = user
 
 @app.route('/')
 #landing page; should have functionality on finding a tournament to bet on, or logging in for creating/overviewing owned tournaments.
 def index():
+  if not g.user:
+    return redirect('/login')
   return "<h1>Faka ambre</h1>"
 
-@app.route('/login')
-#login page; add functionality to go to sign-in page, add functionality to check login, and save current logged in account (hard?)
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
+  if request.method == 'POST':
+    print("in post")
+    session.pop('user_id', None)
+
+    username = request.form['username']
+    password = request.form['password']
+
+    print(username)
+
+    user = [x for x in users if x.username == username][0]
+    if user and user.password == password:
+      session['user_id'] = user.id
+      return redirect("/")
+
+    return redirect('/login')
+
+  else: 
+    return render_template('login.html')
+
+#@app.route('/login')
+#login page; add functionality to go to sign-up page, add functionality to check login, and save current logged in account (hard?)
+#def login():
+#  return render_template('login.html')
 
 #TODO1: build page for creating account, POST account into database, w/ encrypted password
-#@app.route('/sign_up')
+@app.route('/signup', METHODS=['POST', 'GET'])
+def signup():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+
+    if check_existence(username):
+      return "account already exists"
+
+    else:
+      create_record(accounts, [username, fernet.encrypt(password.encode()), email])
+
+  return render_template('signup.html')
 
 #TODO2: build landing page post-login, overviewing tournaments, allowing to create tournament.
 #@app.route('/tournaments')
@@ -30,7 +87,7 @@ def login():
 db = connect_db()
 
 #establish encryption
-fernet = encryption()
+#fernet = encryption()
 
 #fernet.encrypt(password.encode())
 #fernet.decrypt(encPassword).decode()
@@ -42,5 +99,5 @@ tournaments = db.Tournaments
 
 
 
-create_record(accounts, ['BitchBoy2','No', '789'])
+#create_record(accounts, ['BitchBoy2','No', '789'])
 
