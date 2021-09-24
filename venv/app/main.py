8d5622a140338413e6f0fe856315d7e3013915ea
kjellvs <64495619+kjellvs@users.noreply.github.com>
2021-09-24 14:00:35 +0200

    framework + nav

--- venv/app/main.py
+++ venv/app/main.py
@@ -31,13 +31,27 @@ def before_request():
 @app.route('/')
 #landing page; should have functionality on finding a tournament to bet on, or logging in for creating/overviewing owned tournaments.
 def index():
-  if not g.user:
-    return redirect('/login')
-  return "<h1>Faka ambre</h1>"
+  return render_template('home.html')
+
+@app.route('/navbar')
+def navbar():
+  if g.user:
+    current_user = g.user.username
+  else:
+    current_user = False
+  return render_template('navbar.html', current_user=current_user)
+
+@app.route('/logout')
+def logout():
+  print(session['user_id'] + 'logged out')
+  session.pop('user_id', None)
+  return redirect('/login')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
+   
     session.pop('user_id', None)
 
     username = request.form['username']
@@ -45,15 +59,16 @@ def login():
 
     #find instance associated to given name, if no instance is found NONE is returned
     login_user = accounts.find_one({"name": username})
-    user = User(login_user['_id'],login_user['name'],login_user['password'])
     
-    #if we have a user, and the decrypted passwords match then save new user id and redirect to tournaments page;
+        #if we have a user, and the decrypted passwords match then save new user id and redirect to tournaments page;
     if login_user and (fernet.decrypt(login_user["password"]).decode() == password):
+      user = User(login_user['_id'],login_user['name'],login_user['password'])
       session['user_id'] = str(user.id)
-      return redirect("/tournaments")
+      return redirect("/")
 
     #else, redirect to login page again
-    return redirect('/login')
+    else:
+      return render_template('login.html', failed = True)
 
   else: 
     return render_template('login.html')
@@ -75,16 +90,32 @@ def signup():
 
   return render_template('signup.html')
 
-#TODO2: build landing page post-login, overviewing tournaments, allowing to create tournament.
-#@app.route('/tournaments')
+#TODO2: build landing page post-login, overviewing tournaments
+@app.route('/tournaments')
+def tournaments():
+  if not g.user:
+    return redirect('/login')
+  return render_template('tournaments.html')
+
+#TODO2.5: create tournament page
+@app.route('/create_tournament')
+def create_tournaments():
+  if not g.user:
+    return redirect('/login')
+  return render_template('createTournament.html')
 
 #TODO3: build tournament overview (i.e. analytics dashboard, settings, leaderboards) for selected tournament in TODO2, based on logged in account (Hard!)
-#@app.route('/tournaments/{tournament_name}')
+@app.route('/tournaments/{tournament_name}')
+def tournament_overview():
+  if not g.user:
+    return redirect('/login')
+  return render_template('tournamentOverview.html', tournament_name = "clicked tournament")
 
 #TODO4: build betting page for selected tournament, pulls active (in the next 7 days) matches, 
 #and optionality to fill in scores which can be posted (only if owner of tournament!)
-#@app.route('/bet/{tournament_name}')
-
+@app.route('/bet/{tournament_name}')
+def bet_tournament():
+  return render_template('betTournament.html', tournament_name = "clicked tournament")
 
 #fernet.encrypt(password.encode())
 
